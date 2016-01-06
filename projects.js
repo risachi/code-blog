cf9dab340083c20a0f957e692325d1ce27bf730d
Lisa Hackenberger <risachi@gmail.com>
2016-01-06 00:31:18 -0800

    Taking code from pair project, modifying it for my needs
    I’m taking files from the pair project (the blog) and modifying the
    code so that it suits this project. To truly write it from scratch is
    somewhat daunting .. at least this way I have a template/some
    structure. projects.js will contain the array of objects that I will be
    using - the objects are each portfolio piece.

---
+++ projects.js
@@ -0,0 +1,20 @@
+var projects = [
+  {
+    title:       'Quisitive',
+    thumbnail:
+    image:   'http://www.imdb.com/name/nm0000102/',
+    description:        '<p>Bacon ipsum dolor amet do ex andouille minim</p>',
+  },
+  {
+    title:       'Baseball Ipsum',
+    thumbnail:    'sports',
+    image:      'Felix Hernandez',
+    description:   'http://www.baseball-reference.com/players/h/hernafe02.shtml',
+  },
+  {
+    title:       'Cat Ipsum',
+    thumbnail:    'pets',
+    image:      'Meow Meow',
+    description:   'https://http.cat/',
+  },
+];
