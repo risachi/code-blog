3c283ca3f157fbf4b8428bc446d819d1c35e8542
Lisa Hackenberger <risachi@gmail.com>
2016-01-07 23:34:26 -0800

    pulling the data in and it is displaying on the site. sweet.

--- projects.js
+++ projects.js
@@ -1,4 +1,4 @@
-var projects = [
+var posts = [
   {
     title: 'About Me',
     thumbnail: 'old-pieces/thumbnails/aboutMe.png',
@@ -14,9 +14,17 @@ var projects = [
     description: '<p>A handy tool to predict how long the lines will be at your favorite donut shop. Just stalk your favorite shop and plug in the numbers; the calculator will tell you how many customers to expect each hour!</p>'
   },
   {
-    title: 'Cat Ipsum',
-    thumbnail: 'pets',
-    image: 'Meow Meow',
-    description: 'https://http.cat/',
+    title: 'Vote Trakr',
+    thumbnail: 'old-pieces/thumbnails/voteTrakr.png',
+    image: '',
+    oneLiner: 'Cute pandas or cute food? Your choice.',
+    description: '<p>Did you know that there is a shortage of data on whether people prefer adorable red pandas, or aesthetically pleasing food items? This application aims to correct this problem. You\'re welcome.</p>'
   },
+  {
+    title: 'Discover Portland',
+    thumbnail: 'old-pieces/thumbnails/discoverPortland.png',
+    image: '',
+    oneLiner: 'Plan your day in The City of Roses.',
+    description: '<p>This application recommends different activities in PDX to different people. Swell!</p>'
+  }
 ];
--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+var projects = [];
+
 function Project (piece) {
   this.title = piece.title;
   this.thumbnail = piece.thumbnail;
@@ -9,34 +11,18 @@ function Project (piece) {
 Project.prototype.toHtml = function() {
   var $newProject = $('project.template').clone();
 
-  $newProject.data('category', this.category);
+  $newProject.find('h1').html(this.title);
+  $newProject.find('#thumbnail').html(this.thumbnail);
+  $newProject.find('#oneLiner').html(this.oneLiner);
+  $newProject.find('#image').html(this.image);
+  $newProject.find('#description').html(this.description);
 
-  $newArticle.find('h1').html(this.title);
-  $newArticle.find('thumbnail').html(this.thumbnail);
-  $newArticle.find('oneLiner').html(this.oneLiner);
-  $newArticle.find('image').html(this.image);
+  $newProject.append('<hr>');
 
-  // Include the publication date as a 'title' attribute to show on hover:
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-
-  // Display the date as a relative number of "days ago":
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-
-  $newArticle.append('<hr>');
-
-  // DONE: This cloned article is no longer a template, so we should remove that class...
-  $newArticle.removeClass('template');
-  return $newArticle;
+  $newProject.removeClass('template');
+  return $newProject;
 };
 
-rawData.sort(function(a,b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
-
-rawData.forEach(function(ele) {
-  articles.push(new Article(ele));
-});
-
-articles.forEach(function(a){
-  $('#articles').append(a.toHtml());
+posts.forEach(function(a) {
+  $('#projects').append((new Project(a)).toHtml());
 });
