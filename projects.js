614032e0d827b6b8b5a66ede291d472ecf932c23
Lisa Hackenberger <risachi@gmail.com>
2016-01-08 23:39:04 -0800

    Look at all the jQuery versions I removed :D

--- projects.js
+++ projects.js
@@ -2,29 +2,29 @@ var posts = [
   {
     title: 'About Me',
     thumbnail: 'old-pieces/thumbnails/aboutMe.png',
-    image: '',
+    image: 'http://placehold.it/350x350',
     oneLiner: 'Ruffly done "About Me" page',
     description: '<p>An "About Me" page for the household dog, who is obsessed with cats. All content is his; I was his medium due to his lack of opposable thumbs.</p>'
   },
   {
     title: 'Donut Shop Forecaster',
     thumbnail: 'old-pieces/thumbnails/donutShop.png',
-    image: '',
+    image: 'http://placehold.it/350x350',
     oneLiner: 'Which donut shops will have long lines today?',
     description: '<p>A handy tool to predict how long the lines will be at your favorite donut shop. Just stalk your favorite shop and plug in the numbers; the calculator will tell you how many customers to expect each hour!</p>'
   },
   {
     title: 'Vote Trakr',
     thumbnail: 'old-pieces/thumbnails/voteTrakr.png',
-    image: '',
+    image: 'http://placehold.it/350x350',
     oneLiner: 'Cute pandas or cute food? Your choice.',
     description: '<p>Did you know that there is a shortage of data on whether people prefer adorable red pandas, or aesthetically pleasing food items? This application aims to correct this problem. You\'re welcome.</p>'
   },
   {
     title: 'Discover Portland',
     thumbnail: 'old-pieces/thumbnails/discoverPortland.png',
-    image: '',
+    image: 'http://placehold.it/350x350',
     oneLiner: 'Plan your day in The City of Roses.',
-    description: '<p>This application recommends different activities in PDX to different people. Swell!</p>'
+    description: '<p>This application recommends different activities in PDX to different people. Swell! Final project by pdx201n1\'s students.</p>'
   }
 ];
--- scripts.js
+++ scripts.js
@@ -1,28 +1,31 @@
-var projects = [];
+$(function() {
 
-function Project (piece) {
-  this.title = piece.title;
-  this.thumbnail = piece.thumbnail;
-  this.image = piece.image;
-  this.oneLiner = piece.oneLiner;
-  this.description = piece.description;
-}
+  function Project(piece) {
+    this.title = piece.title;
+    this.thumbnail = piece.thumbnail;
+    this.image = piece.image;
+    this.oneLiner = piece.oneLiner;
+    this.description = piece.description;
+  }
 
-Project.prototype.toHtml = function() {
-  var $newProject = $('project.template').clone();
+  Project.prototype.toHtml = function() {
+    var $newProject = $('article.template').clone();
 
-  $newProject.find('h1').html(this.title);
-  $newProject.find('#thumbnail').html(this.thumbnail);
-  $newProject.find('#oneLiner').html(this.oneLiner);
-  $newProject.find('#image').html(this.image);
-  $newProject.find('#description').html(this.description);
+    $newProject.find('h2').html(this.title);
+    $newProject.find('#thumbnail').html(this.thumbnail);
+    $newProject.find('#oneLiner').html(this.oneLiner);
+    $newProject.find('div > a').attr('href', this.image);
+    $newProject.find('a > img').attr('alt', this.title);
+    $newProject.find('div > a').attr('title', this.description);
 
-  $newProject.append('<hr>');
+    $newProject.removeClass('template');
+    return $newProject;
+  };
 
-  $newProject.removeClass('template');
-  return $newProject;
-};
+  posts.forEach(function(a) {
+    $('#projects').append((new Project(a)).toHtml());
+  });
 
-posts.forEach(function(a) {
-  $('#projects').append((new Project(a)).toHtml());
+  $('.template').hide();
+  $("a[rel^='prettyPhoto']").prettyPhoto();
 });
