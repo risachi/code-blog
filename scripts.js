96d75b58a0b98763b5820a514f78456d2f3edc21
Lisa Hackenberger <risachi@gmail.com>
2016-01-09 00:44:14 -0800

    Font style, add content, styling changes

--- scripts.js
+++ scripts.js
@@ -12,11 +12,12 @@ $(function() {
     var $newProject = $('article.template').clone();
 
     $newProject.find('h2').html(this.title);
-    $newProject.find('#thumbnail').html(this.thumbnail);
     $newProject.find('#oneLiner').html(this.oneLiner);
-    $newProject.find('div > a').attr('href', this.image);
     $newProject.find('a > img').attr('alt', this.title);
+    $newProject.find('a > img').attr('src', this.thumbnail);
+    $newProject.find('div > a').attr('href', this.image);
     $newProject.find('div > a').attr('title', this.description);
+
 
     $newProject.removeClass('template');
     return $newProject;
