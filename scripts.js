336e259110c6b4f0910e479151ada994c24d5099
Lisa Hackenberger <risachi@gmail.com>
2016-01-10 16:18:13 -0800

    extract method age
    This is how a ‘pro’ would refactor…

--- scripts.js
+++ scripts.js
@@ -9,8 +9,13 @@ $(function() {
     this.publishedOn = piece.publishedOn;
   }
 
-  Project.prototype.toHtml = function() {
+  Project.prototype.age = function() {
     var MILLIS_IN_DAYS = 60 * 60 * 24 * 1000;
+    return parseInt((new Date() - new Date(this.publishedOn)) / MILLIS_IN_DAYS);
+  },
+
+  Project.prototype.toHtml = function() {
+
     var $newProject = $('article.template').clone();
 
     $newProject.find('h2').html(this.title);
@@ -22,7 +27,7 @@ $(function() {
 
     $newProject.find('time').html('title', this.publishedOn);
 
-    $newProject.find('time').html('posted ' + parseInt((new Date() - new Date(this.publishedOn)) / MILLIS_IN_DAYS) + ' days ago');
+    $newProject.find('time').html('posted ' + this.age() + ' days ago');
 
 
     $newProject.removeClass('template');
