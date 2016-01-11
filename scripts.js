bd892ab979a589326b4d3314e968331b5cb1bcdd
Lisa Hackenberger <risachi@gmail.com>
2016-01-10 16:31:15 -0800

    Start writing the tab functionality

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
 $(function() {
+  $('nav').on('click', function(e) {
+    console.log(e.target);
+    // 1. Find out the tab number
+    // 2. Get the content tab
+    // 4. Hide'em all
+    // 3. Show the one we want
+  });
 
   function Project(piece) {
     this.title = piece.title;
@@ -12,11 +19,11 @@ $(function() {
   Project.prototype.age = function() {
     var MILLIS_IN_DAYS = 60 * 60 * 24 * 1000;
     return parseInt((new Date() - new Date(this.publishedOn)) / MILLIS_IN_DAYS);
-  },
+  };
 
   Project.prototype.toHtml = function() {
-
     var $newProject = $('article.template').clone();
+    $newProject.removeClass('template');
 
     $newProject.find('h2').html(this.title);
     $newProject.find('#oneLiner').html(this.oneLiner);
@@ -24,13 +31,8 @@ $(function() {
     $newProject.find('a > img').attr('src', this.thumbnail);
     $newProject.find('div > a').attr('href', this.image);
     $newProject.find('div > a').attr('title', this.description);
-
     $newProject.find('time').html('title', this.publishedOn);
-
     $newProject.find('time').html('posted ' + this.age() + ' days ago');
-
-
-    $newProject.removeClass('template');
     return $newProject;
   };
 
